ba7a689f4343d09acbc6f75772e04cf7d5378481
JKnight3517 <justin@thevessale.com>
2024-09-05 08:18:15 -0700

    add initial api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "vessale": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
