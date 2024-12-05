97a4725e3274488c839a22bdc9abcba26be3a5d9
JKnight3517 <justin@thevessale.com>
2024-12-04 19:40:58 -0600

    Final wire up

--- src/hooks/useCurrentRestaurant.ts
+++ src/hooks/useCurrentRestaurant.ts
@@ -1,4 +1,6 @@
-// src/hooks/useCurrentRestaurant.ts
+// File used to find the current restaurant based on the subdomain 
+// Uses a default for the dev subdomain
+
 import { DataStore } from '@aws-amplify/datastore';
 import { Restaurant } from '../models';
 import { useState, useEffect } from 'react';
@@ -11,8 +13,10 @@ export function useCurrentRestaurant() {
   useEffect(() => {
     async function fetchRestaurant() {
       try {
-        // Get subdomain from window.location.hostname
-        const subdomain = window.location.hostname.split('.')[0];
+        const hostname = window.location.hostname;
+        const subdomain = hostname.includes('amplifyapp.com') 
+            ? 'dev-restaurant'  // Default dev subdomain
+            : hostname.split('.')[0];
         
         const [restaurantData] = await DataStore.query(Restaurant, r => 
           r.subdomain.eq(subdomain)
